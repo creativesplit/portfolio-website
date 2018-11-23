004a1bb1e2dd3b7eba9ca5bf0e8bc26d43d4a975
Q Hardy <qhardy1@gmail.com>
2018-11-23 17:38:28 -0500

    footer, footer links, grid positioning, some CSS

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './Footer.css';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Footer extends Component {
   render() {
     return (
-     <div>
-     {/* <Link>Github</Link>
-     <Link>LinkedIn</Link>
-     <Link>Resume</Link> */}
-       
-     </div>
+     <nav className='links' >
+      <Link to="https://www.linkedin.com/in/mcewen-hardy-49506622/" className="one"><img classname='four' src="#"/>LinkedIn</Link>
+      <Link to="https://github.com/creativesplit" className="item4"><img classname='four' src="#"/>Github</Link>
+      <Link to="http://foolish-liquid.surge.sh/" className="item4"><img classname='four' src="#"/>Resume</Link>
+     </nav>
     );
   }
 }
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 class Portfolio extends Component {
   render() {
     return (
-     <div className='grid-container'>
-        <Link to="http://karaokeroulette.com" className='grid-item'><img src='https://i.imgur.com/EvMyZe4.png' alt='Towers of Hanoi'/></Link>
-        <Link to="http://my-music-dna.com" className='grid-item'><img src='https://i.imgur.com/TXURZ9d.jpg' alt='Music DNA'/></Link>
-        <Link to="http://karaokeroulette.com" className='grid-item'><img src='https://i.imgur.com/yX89qjr.jpg' alt='Apartment Guidepost'/></Link>
-        <Link to="http://foolish-liquid.surge.sh/" className='grid-item'><img src='https://i.imgur.com/XFMkKjR.jpg' alt='Karaoke Roulette'/></Link>
+     <div className="grid-container">
+        <Link to="http://karaokeroulette.com" className="item1"><img classname='one' src="#"/>1</Link>
+        <Link to="http://my-music-dna.com" className="item2"><img classname='two' src="#"/>2</Link>
+        <Link to="http://karaokeroulette.com" className="item3"><img classname='three' src="#"/>3</Link>
+        <Link to="http://foolish-liquid.surge.sh/" className="item4"><img classname='four' src="#"/>4</Link>
      </div>
     );
   }
